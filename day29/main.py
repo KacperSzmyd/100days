08017b6f7e72c50a0c4fb1225b2592e778e954e9
Kacper <k.g.szmyd@gmail.com>
2025-01-20 16:07:43 +0100

    searching for credentials implemented

--- day29/main.py
+++ day29/main.py
@@ -118,6 +118,32 @@ def save_password():
                 password_input.delete(0, END)
 
 
+# ---------------------------- SEARCH DATA ------------------------------- #
+
+
+def search_password():
+    website = website_input.get()
+    if website == "":
+        messagebox.showerror(title="Oops", message="Please do not leave empty field!")
+    else:
+        try:
+            with open("data/passwords.json", "r") as file:
+                data = json.load(file)
+
+        except FileNotFoundError:
+            messagebox.showerror(title="Error", message="There is no password file yet")
+        else:
+            if website in data:
+                username = data[website]["email"]
+                password = data[website]["password"]
+                messagebox.showinfo(
+                    title=f"{website} credentials",
+                    message=f"Email: {username}\nPassword: {password}",
+                )
+            else:
+                messagebox.showerror(title="Error", message=f"No details for {website}")
+
+
 # ---------------------------- UI SETUP ------------------------------- #
 
 window = Tk()
@@ -133,19 +159,22 @@ canvas.grid(column=1, row=0)
 
 website_label = Label(text="Website:")
 website_label.grid(column=0, row=1)
-website_input = Entry(width=39)
-website_input.grid(column=1, row=1, columnspan=2)
+website_input = Entry(width=31)
+website_input.grid(column=1, row=1)
 website_input.focus()
+
+search_button = Button(text="Search", command=search_password, width=14)
+search_button.grid(column=2, row=1)
 
 email_label = Label(text="Email/Username:")
 email_label.grid(column=0, row=2)
-email_input = Entry(width=39)
-email_input.grid(column=1, row=2, columnspan=2)
+email_input = Entry(width=31)
+email_input.grid(column=1, row=2)
 email_input.insert(END, string="k.g.szmyd@gmail.com")
 
 password_label = Label(text="Password:")
 password_label.grid(column=0, row=3)
-password_input = Entry(width=21)
+password_input = Entry(width=31)
 password_input.grid(column=1, row=3)
 
 generate_button = Button(text="Generate password", command=generate_password)
@@ -153,7 +182,7 @@ generate_button.grid(column=2, row=3)
 
 add_button = Button(
     text="Add",
-    width=36,
+    width=43,
     command=save_password,
 )
 add_button.grid(column=1, row=4, columnspan=2)
